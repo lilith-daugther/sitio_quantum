bd43a80ce699933b82a5c8b0bbe102b6c748ed26
lilith_daugther <kaithlyn.mendez@gmail.com>
2024-05-18 18:27:57 -0600

    se cambió el modelo

--- sitio_quantum/parque_quantum/models.py
+++ sitio_quantum/parque_quantum/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-class Actividad(models.Model):
+class MiModelo(models.Model):
     nombre = models.CharField(max_length=100)
-    imagen = models.ImageField(upload_to='imagenes_actividades/')  # Asegúrate de configurar MEDIA_ROOT y MEDIA_URL
-    fecha = models.DateField()
     descripcion = models.TextField()
+    fecha = models.DateField()
+    imagen = models.ImageField(upload_to='imagenes/')
 
     def __str__(self):
         return self.nombre
-    
-    
