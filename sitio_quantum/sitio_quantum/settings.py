1ade2094380b70ffffb9146036903d72e8dca8e8
lilith_daugther <kaithlyn.mendez@gmail.com>
2024-05-29 19:21:56 -0600

    actualización de requirements.txt

--- sitio_quantum/sitio_quantum/settings.py
+++ sitio_quantum/sitio_quantum/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 
 
